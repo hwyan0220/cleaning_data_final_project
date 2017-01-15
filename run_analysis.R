b9b2a5547dfe671abddc3ca61832d3b0f577f99a
hwyan0220 <hwyan0220@gmail.com>
2017-01-15 18:17:31 +1100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,4 @@
 rm(list = ls())
-setwd('/Users/norman/desktop/data science course/UCI HAR Dataset/')
 library(dplyr)
 
 
@@ -8,34 +7,29 @@ library(dplyr)
 tr <- read.table('./train/X_train.txt')
 trl <- read.table('./train/y_train.txt')
 trs <- read.table('./train/subject_train.txt')
+
 # load feature label
 fl <- read.table('./features.txt', stringsAsFactors = F)
 # load activity label
-al <- read.table('./activity_labels.txt')
-# clean activity label for training data
-trl <- merge(trl, al, by = 'V1', all = T)
-trl <- trl[,2]
+al <- read.table('./activity_labels.txt', stringsAsFactors = F)
+colnames(al) <- c('activity', 'activity_desc')
 # create label vector for dataset
 names <- c('subject', 'activity', fl$V2)
 # training data
 train <- cbind(trs, trl, tr)
 names(train) <- names
 
-
 # process testing dataset
 # load test data, test activity label and test subject
 te <- read.table('./test/X_test.txt')
 tel <- read.table('./test/y_test.txt')
 tes <- read.table('./test/subject_test.txt')
-# clean activity label for testing data
-tel <- merge(tel, al, by = 'V1', all = T)
-tel <- tel[,2]
 # testing data
 test <- cbind(tes, tel, te)
 names(test) <- names
 
 # clean up workspace
-rm(list = c('al', 'fl', 'te', 'tel', 'tes', 'tr', 'trs', 'trl'))
+rm(list = c('fl', 'te', 'tel', 'tes', 'tr', 'trs', 'trl'))
 
 # QUESTION NUMBER 1 - combine train and test into one data set
 dat <- rbind(train, test)
@@ -45,16 +39,15 @@ dat1 <- dat[,(grepl('mean', names)|grepl('std', names))]
 dat1 <- cbind(dat[,c(1:2)], dat1)
 
 # QUESTION NUMBER 3 - Uses Descriptive activity names to name the activities in the data set
-# this is done
+dat1 <- merge(dat1, al, by = 'activity', all.x = TRUE)
 
 # QUESTION NUMBER 4 - Appropriately labels the data set with descriptive variable names. 
 # this is done
 
 # QUESTION NUMBER 5 - create tidy data set with the average of each variable for each activity and each subject
-dat1$subject <- as.factor(dat1$subject)
-
+dat1 <- dat1[,c(2:82)]
 dat2 <- dat1                          %>%
-        group_by(subject, activity)   %>%
+        group_by(subject, activity_desc)   %>%
         summarise_each(funs(mean))
 
 write.table(dat2, 'tidydata.txt', row.names = FALSE)
